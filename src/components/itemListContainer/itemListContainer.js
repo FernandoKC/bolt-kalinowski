13c3f2f003692584f4a6f88999107dd68b86b95e
FernandoKC <fernandokwcunha@gmail.com>
2021-09-25 16:55:15 -0300

    Entrega clase 4

---
+++ src/components/itemListContainer/itemListContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ItemListContainer = (props) => {
+    return (
+        <>
+        <h1>Welcome {props.greeting}</h1>
+        </>
+    );
+};
+
+export default ItemListContainer;

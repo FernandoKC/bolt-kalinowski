893c9be34f697d70da13d3ac5e60a7c4ff7ec2c7
FernandoKC <fernandokwcunha@gmail.com>
2021-10-12 23:52:01 -0300

    creating detail.js to call to api in specific ID

---
+++ src/views/Detail/Detail.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import React, {useState, useEffect} from 'react';
+import ItemDetail from "../../components/itemDetail/itemDetail";
+
+const Detail = ({ match }) => {
+    let itemID = match.params.id;
+    const [item, setItem] = useState({});
+    const min = 1;
+    const max = 10;
+
+    useEffect(() => {
+        axios(`https://fakestoreapi.com/products/${itemID}`).then((res) =>
+            setItem(res.data)
+        );
+    }, [itemID])
+
+    const rand = min + Math.random() * (max - min);
+    item["initial"] = 1;
+    item["stock"] = rand.toFixed();
+    
+    return (
+        <div className="CardContainer">
+              <div className="itemDiv" key={item.id}>
+                  <ItemDetail data={item}/>
+              </div>
+        </div>
+      );
+}
+
+export default Detail

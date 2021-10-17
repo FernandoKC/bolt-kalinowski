0a78e0cd4dcf051a1687b99287497400b72cfca9
FernandoKC <fernandokwcunha@gmail.com>
2021-10-17 19:55:21 -0300

    Fix itemlist import

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Navbar from "./components/navbar/navbar"
 
 import Detail from "./views/Detail/Detail";
-import ItemList from "./components/itemList/itemList";
+import ItemList from "./views/ItemList/ItemList";
 
 
 const App = () => {

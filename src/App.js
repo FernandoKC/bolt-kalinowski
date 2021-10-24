a78ae8cc4a407c35db6d39238957ccefad3c5731
FernandoKC <fernandokwcunha@gmail.com>
2021-10-24 18:44:38 -0300

    Import itemsprovider

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import Navbar from "./components/navbar/navbar"
+import Navbar from "./components/navbar/navbar";
 
 import Detail from "./views/Detail/Detail";
 import ItemList from "./views/ItemList/ItemList";
@@ -10,25 +10,25 @@ import Category from "./views/Category/Category";
 import Home from "./views/Home/Home";
 import Cart from "./views/Cart/Cart";
 
+import { ItemsProvider } from "./CartContext";
 
 const App = () => {
-
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <Switch>
-          <Route path="/cart" component={Cart}/>
-          <Route path="/ItemList" component={ItemList}/>
-          <Route path="/Detail/:id" component={Detail}/>
-          <Route path="/Category/:id" component={Category}/>
-          <Route path="/" component={Home}/>
-        </Switch>
-      </div>
-    </Router>
+    <ItemsProvider>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route path="/cart" component={Cart} />
+            <Route path="/ItemList" component={ItemList} />
+            <Route path="/Detail/:id" component={Detail} />
+            <Route path="/Category/:id" component={Category} />
+            <Route path="/" component={Home} />
+          </Switch>
+        </div>
+      </Router>
+    </ItemsProvider>
   );
-
 };
 
 export default App;
-

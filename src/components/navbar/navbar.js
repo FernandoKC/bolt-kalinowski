cdde7845853d3ca583b7df241dc4acb1dc03468b
FernandoKC <fernandokwcunha@gmail.com>
2021-10-27 21:30:43 -0300

    add link to navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,33 +1,37 @@
 import React, { useContext } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/container";
-import Nav from "react-bootstrap/Nav";
 import CartWidget from "../cartWidget/cartWidget";
 import CategoryList from "../categoryList/categoryList";
 import { ItemsContext } from "../../CartContext";
+import { Link } from "react-router-dom";
 
 const NavBar = (props) => {
-  const [cartItem, , clear, , removeItem] = useContext(ItemsContext);
+  const [cartItem, , , , ] = useContext(ItemsContext);
   return (
     <div>
       <Navbar bg="dark" variant="dark" expand="xl" sticky="top">
         <Container fluid>
-          <Navbar.Brand href="/">Kali Store</Navbar.Brand>
+              <Link to={`/`} className="navbar-brand" tabindex="0">
+                Kali Store
+              </Link>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="/ItemList">All Items </Nav.Link>
+            <div className="me-auto navbar-nav">
+              <Link to={`/ItemList`} role="button" className="nav-link" tabindex="0">
+                All Items
+              </Link>
               <CategoryList />
-            
-            {cartItem.length === 0 ? (
-              console.log("a")
-            ) : (
-              <>
-                <CartWidget />
-                <Navbar.Brand>{cartItem.length} item(s)</Navbar.Brand>
-              </>
-            )}
-            </Nav>
+
+              {cartItem.length === 0 ? (
+                console.log("a")
+              ) : (
+                <>
+                  <CartWidget />
+                  <Navbar.Brand>{cartItem.length} item(s)</Navbar.Brand>
+                </>
+              )}
+            </div>
           </Navbar.Collapse>
         </Container>
       </Navbar>
